6f41b4c51988d6ba1b99ea7de6ccb002e68755d1
daniel <dannykl@hotmail.co.uk>
2017-04-03 11:30:16 +0100

    open shift is added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Daniel Niguse',
+      version='1.0',
+      description='OpenShift Based App',
+      author='Daniel',
+      author_email='example@example.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask','flask-wtf','flask-babel','markdown','flup','Flask-PyMongo','Unirest','bokeh','numpy','flask-navigation','MarkupSafe'],
+      )
